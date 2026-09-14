e6804b92fb1d824a46d97eab86127a900bbba59e
Ziednaga <tmicebluecosmic@gmail.com>
2026-09-13 22:03:37 -0300

    Revert "cpufreq: s5e3830: undervolt CPUCL0/1 by 10% and fix the top dm-table row"
    This reverts commit 0d7d6015a372b793137358e71b6ce8299a95fc79.

--- kernel_build/buildnoavbp.sh
+++ kernel_build/buildnoavbp.sh
@@ -151,11 +151,7 @@ find . | cpio --quiet -o -H newc -R root:root | lz4 -9cl > ../ramdisk_dlkm.lz4
 cd ../ramdisk_platform
 find . | cpio --quiet -o -H newc -R root:root | lz4 -9cl > ../ramdisk_platform.lz4
 cd ..
-{
-    echo "buildtime_bootconfig=enable"
-    echo 'kernel.cmupmucal.margin_cpucl0 = "-10"'
-    echo 'kernel.cmupmucal.margin_cpucl1 = "-10"'
-} > bootconfig
+echo "buildtime_bootconfig=enable" > bootconfig
 
 $MKBOOTIMG --header_version 4 \
     --vendor_boot "$OUT_VENDORBOOTIMG" \
